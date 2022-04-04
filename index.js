13816a9d20c2bdd645c09204149de329cb43b1af
Lucifer <christiandc757@gmail.com>
2022-04-04 17:01:42 -0500

    Portafolio Finalizado

---
+++ index.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.about');
+ScrollReveal().reveal('.skills__title');
+ScrollReveal().reveal('.skills');
+ScrollReveal().reveal('.contact__title');
+ScrollReveal().reveal('.form-register')
+ScrollReveal().reveal('.footer')
